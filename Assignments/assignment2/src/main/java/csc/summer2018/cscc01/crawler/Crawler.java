35669445b55dade77fa256660cb7115e56422f20
Kalindu De Costa <kalindu.decosta@mail.utoronto.ca>
2018-07-09 18:55:48 -0400

    initialized Crawler.java and Constants.java

---
+++ Assignments/assignment2/src/main/java/csc/summer2018/cscc01/crawler/Crawler.java
@@ -0,0 +1,5 @@
+package csc.summer2018.cscc01.crawler;
+
+public class Crawler {
+
+}
---
+++ Assignments/assignment2/src/main/java/csc/summer2018/cscc01/util/Constants.java
@@ -0,0 +1,7 @@
+package csc.summer2018.cscc01.util;
+
+import edu.uci.ics.crawler4j.crawler.WebCrawler;
+
+public class Constants extends WebCrawler {
+
+}
